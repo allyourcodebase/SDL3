540fe66af2ca5050fc1aabdf7a19cf462ea0312e
Jay Petacat <jay@jayschwa.net>
2026-09-05 17:24:49 -0600

    Upgrade to SDL 3.4.16 (#31)
    * Remove redundant SDL version from vendor info string
    
    SDL version is already included in the Zig wrapper package's version.
    
    * Upgrade to SDL 3.4.16

--- build.zig
+++ build.zig
@@ -59,10 +59,9 @@ pub fn build(b: *std.Build) !void {
         },
         .static => lib.root_module.addCMacro("SDL_STATIC_LIB", "1"),
     }
-    lib.root_module.addCMacro("SDL_VENDOR_INFO", std.fmt.comptimePrint("\"{s} {s} (SDL {s})\"", .{
+    lib.root_module.addCMacro("SDL_VENDOR_INFO", std.fmt.comptimePrint("\"{s} {s}\"", .{
         "https://github.com/allyourcodebase/SDL",
         build_zon.version,
-        build_zon.dependencies.sdl.version,
     }));
     lib.installHeadersDirectory(upstream.path("include/SDL3"), "SDL3", .{});
     b.installArtifact(lib);
